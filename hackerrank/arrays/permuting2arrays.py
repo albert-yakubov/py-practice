89f098181d50b65b55c69196919bc2e90b0d3a13
Albert Yakubov <mrayakubov@hotmail.com>
2020-05-08 16:36:33 -0600

    Create permuting2arrays.py

---
+++ hackerrank/arrays/permuting2arrays.py
@@ -0,0 +1,17 @@
+def twoArrays(k, A, B):
+    A.sort()
+    B.sort(reverse=True)
+
+    for x in range(len(A)):
+        if( A[x] + B[x] < k ):
+            return "NO"
+
+    return "YES"
+k = 1
+A = [1, 2, 2, 1]
+B = [3, 3, 3, 4]
+print(twoArrays(k,A,B))
+k = 2
+C = [2, 1, 3]
+D = [7, 8, 9]
+print(twoArrays(k,C,D))
