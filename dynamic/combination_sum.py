ad420f14146e19ec1c8d6d4b0f9b141ae4e3ea31
Albert Yakubov <mrayakubov@hotmail.com>
2020-06-25 10:03:23 -0600

    Update combination_sum.py
    more on repl.it

--- dynamic/combination_sum.py
+++ dynamic/combination_sum.py
@@ -1,9 +1,21 @@
 
 class Solution:
-    nums = [1, 2, 3]
-    target = 4
+    # nums = [1, 2, 3]
+    # target = 4
+
+    def findPairs(mylist, K):
+        res = []
+        while mylist:
+            num = mylist.pop()
+            diff = K - num
+            if diff in mylist:
+                res.append((diff, num))
+
+        res.reverse()
+        return res
     def combinationSum4(self, nums, target):
         dictionary = dict()
+        unique = {}
         def depth_first_searh(target):
             ways = 0
             if target in dictionary:
@@ -13,18 +25,20 @@ class Solution:
             for num in nums:
                 if target >= num:
                     ways += depth_first_searh(target - num)
-            dictionary[target]= ways
-
+            dictionary[target] = ways
+            print(dictionary)
             return ways
-        
+        print(dictionary)
         print(depth_first_searh(target))
         return depth_first_searh(target)
 
+
 if __name__ == "__main__":
-    nums = [1, 2, 3]
+    nums = [1, 2, 3, 4, 5, 6]
     target = Solution().combinationSum4(nums, 4)
 
-    print(target)
-    
+    print(Solution.combinationSum4(0, nums, 5))
+    print(Solution.findPairs(nums, 9))
+
 
 
